78aa9a0a47dc1d8a127da33a133de095be665d26
pedram-farrokhi <pedram.farrokhi.me@gmail.com>
2025-08-03 18:30:40 +0330

    Update 1-21-2.cpp

--- chapter-1/1-21/1-21-2.cpp
+++ chapter-1/1-21/1-21-2.cpp
@@ -1,5 +1,5 @@
-//
-//
+//برنامه ای بنویسید که یک عدد صحیح را از ورودی بخواند و مشخص نماید زوج است یا فرد ؟ 
+//Write a program that reads an integer from the input and determines whether it is even or odd.
 
 #include <iostream>
 using namespace std;
