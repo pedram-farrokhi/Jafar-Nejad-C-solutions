bd035ffc162fa04fb861bdbc487e7910daf31238
pedram-farrokhi <pedram.farrokhi.me@gmail.com>
2025-08-03 18:27:46 +0330

    Create 1-21-2.cpp

---
+++ chapter-1/1-21/1-21-2.cpp
@@ -0,0 +1,13 @@
+//
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << num << " is " << (num % 2 == 0 ? "even" : "odd") << endl;
+    return 0;
+}
