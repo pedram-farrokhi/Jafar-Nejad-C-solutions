62bc66f31ed9468bb3a2fcd75d6a8628e5d72b5a
pedram-farrokhi <pedram.farrokhi.me@gmail.com>
2025-08-04 17:20:05 +0330

    Create 1-21-3.cpp

---
+++ chapter-1/1-21/1-21-3.cpp
@@ -0,0 +1,14 @@
+//دستوراتی بنویسید که متغیر های ایکس و ایگرگ را از نوع عدد صحیح و کاراکتر و متغیر دی را از نوع دابل و ثابت کا را را از نوع عدد صحیح آنسایند با مقدار اولیه 20 تعریف کند 
+//Write statements that define the variables x and y as integers and character, the variable d as double, and the constant k as an unsigned integer with an initial value of 20.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int x;
+  char y;
+  double d;
+  const unsigned int k = 20;
+  return 0;
+}
