07a099a29405872dfd84f29fea8724e71bda93f0
pedram-farrokhi <pedram.farrokhi.me@gmail.com>
2025-08-04 17:44:23 +0330

    Update 1-21-5.cpp

--- chapter-1/1-21/1-21-5.cpp
+++ chapter-1/1-21/1-21-5.cpp
@@ -1,24 +1,40 @@
 //مثال هایی ازانواع در احکام انتساب و تبدیل انواع در عبارات محاسباتی ارائه دهید
 //Give examples of types in assignment statements and type conversions in computational expressions.
 
+//Geeksforgeeks site example
+//https://www.geeksforgeeks.org/cpp/type-conversion-in-c/
+
+//Type conversion in assignment statements
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  return 0;
+}
+
+***
+//Type conversion in computal expressions
+	
 #include <iostream>
 using namespace std;
 
 int main() 
 {
   
-  	// Two variables of different type
-	int i = 10;
+    // Two variables of different type
+    int i = 10;
     char c = 'A';
   
-  	// printing c after manually converting it
-  	cout << (int)c << endl;
+    // printing c after manually converting it
+    cout << (int)c << endl;
   	
-  	// Adding i and c,
-  	int sum = i + c;
+    // Adding i and c,
+    int sum = i + c;
   
-  	// Printing sum
-  	cout << sum;
+    // Printing sum
+    cout << sum;
   	
     return 0;
 }
