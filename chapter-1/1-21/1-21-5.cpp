db7eeeb5fd697de7e2e410bd2bec53cb381551b3
pedram-farrokhi <pedram.farrokhi.me@gmail.com>
2025-08-04 17:31:59 +0330

    Create 1-21-5.cpp

---
+++ chapter-1/1-21/1-21-5.cpp
@@ -0,0 +1,24 @@
+//مثال هایی ازانواع در احکام انتساب و تبدیل انواع در عبارات محاسباتی ارائه دهید
+//Give examples of types in assignment statements and type conversions in computational expressions.
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  
+  	// Two variables of different type
+	int i = 10;
+    char c = 'A';
+  
+  	// printing c after manually converting it
+  	cout << (int)c << endl;
+  	
+  	// Adding i and c,
+  	int sum = i + c;
+  
+  	// Printing sum
+  	cout << sum;
+  	
+    return 0;
+}
