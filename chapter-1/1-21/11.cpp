74e9b7eda2b543dbc094db28011bec145e603b78
pedram-farrokhi <pedram.farrokhi.me@gmail.com>
2025-08-06 01:40:42 +0330

    Update 11.cpp

--- chapter-1/1-21/11.cpp
+++ chapter-1/1-21/11.cpp
@@ -1,1 +1,14 @@
+//برنامه ای بنویسید که مقدار ایکس را از ورودی بخواند و عبارت زیر را محاسبه کند 
+//Write a program that reads the value of x from the input and calculates the following expression
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    double x, y;
+    cout << "Enter value of x: ";
+    cin >> x;
+    y = 1 / ((x*x) + x + 3);
+    cout << "The value of y is: " << y << std::endl;
+    return 0;
+}
