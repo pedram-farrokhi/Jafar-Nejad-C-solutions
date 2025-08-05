0c430300f20cde167d9dea672560cd37a61114ea
pedram-farrokhi <pedram.farrokhi.me@gmail.com>
2025-08-06 01:20:01 +0330

    Update 08.cpp

--- chapter-1/1-21/08.cpp
+++ chapter-1/1-21/08.cpp
@@ -1,1 +1,12 @@
+//
+//
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 8, m = 6;
+    int y = x * 2 < m + 4 ? 4 * m : 8 * m;
+    cout << y << std::endl;
+    return 0;
+}
