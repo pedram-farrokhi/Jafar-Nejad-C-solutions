3612813d19d0d8e841dbaf503088b37a7ae84d8c
pedram-farrokhi <pedram.farrokhi.me@gmail.com>
2025-08-06 01:03:45 +0330

    Create 1-21-7.cpp

---
+++ chapter-1/1-21/1-21-7.cpp
@@ -0,0 +1,20 @@
+//با توجه به مقادیر اعیین شده هر یک از عبارات زیر را ارزیابی کنید 
+//Evaluate each of the following statements according to the values given:
+
+int main() 
+{
+    //first
+    int x = 8, y = 10, m = 6;
+    int t = 0; 
+    int k;
+    
+    //second
+    k = x / y + t + y / -m;
+    cout << k << endl;
+    
+    // third
+    k = x / 4 * y / 2 * m;
+    cout << k << endl;
+    
+    return 0;
+}
