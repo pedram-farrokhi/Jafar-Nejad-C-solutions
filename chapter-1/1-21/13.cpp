6fc050f411ff110c86a9beb2ff203e360bb08e27
pedram-farrokhi <pedram.farrokhi.me@gmail.com>
2025-08-06 01:47:38 +0330

    Update 13.cpp

--- chapter-1/1-21/13.cpp
+++ chapter-1/1-21/13.cpp
@@ -1,1 +1,14 @@
+//برنامه ای بنویسید که وزن کالایی را بر حسب کیلوگرم دریافت کرد و وزن آن را بر حسب گرم در خروجی چاپ کند
+//Write a program that receives the weight of an item in kilograms and prints its weight in grams in the output.
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    double weightInKg, weightInGrams;
+    cout << "Enter the weight in kilograms: ";
+    cin >> weightInKg;
+    weightInGrams = weightInKg * 1000;
+    cout << "The weight in grams is: " << weightInGrams << " grams" << endl;
+    return 0;
+}
