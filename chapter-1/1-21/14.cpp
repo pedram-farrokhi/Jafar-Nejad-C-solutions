6deb1ccbe947157639e97c63966ebb5355671165
pedram-farrokhi <pedram.farrokhi.me@gmail.com>
2025-08-06 01:49:15 +0330

    Update 14.cpp

--- chapter-1/1-21/14.cpp
+++ chapter-1/1-21/14.cpp
@@ -1,1 +1,16 @@
 
+
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    double monthlySalary = 750000;
+    double increasePercent = 12.5;
+    double monthlyIncrease = (monthlySalary * increasePercent) / 100;
+    double yearlyIncreasePerPerson = monthlyIncrease * 12;
+    double totalYearlyIncrease = yearlyIncreasePerPerson * 2;
+    cout << "Total yearly increase in company's cost: " << totalYearlyIncrease << " Rials" << endl;
+    return 0;
+}
