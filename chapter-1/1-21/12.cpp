aa275928f3ab2342b0aae337671ac63c639ad3e4
pedram-farrokhi <pedram.farrokhi.me@gmail.com>
2025-08-06 01:43:56 +0330

    Update 12.cpp

--- chapter-1/1-21/12.cpp
+++ chapter-1/1-21/12.cpp
@@ -1,1 +1,15 @@
+//خروجی دستورات زیر چیست
+//What is the output of the following commands:
 
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int number;
+    number = (1 / 3) * 3;
+    cout << "(1/3) * 3 is equal to " << number << endl;
+    return 0;
+}
+
+//number = 0
