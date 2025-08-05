f5e8175f4bbe02000490a312b999ef237c7f27c3
pedram-farrokhi <pedram.farrokhi.me@gmail.com>
2025-08-06 01:37:13 +0330

    Update 10.cpp

--- chapter-1/1-21/10.cpp
+++ chapter-1/1-21/10.cpp
@@ -1,1 +1,26 @@
+//برنامه ای بنویسید که خروجی زیر را تولید کند
+//Write a program that produces the following output :
 
+
+//version-1
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "The answer to the question of" << endl;
+    cout << "Life, the Universe, and Everything is 42 ." << endl;
+    return 0;
+}
+
+//version-2
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "The answer to the question of\nLife, the Universe, and Everything is 42 ." << endl;
+    return 0;
+}
